0ed6ce3fd85242ec0bcee3921c2ccf62c98ace26
raven <reblackraven@gmail.com>
2022-01-11 00:51:53 +0900

    Feat: implement Queue data structure using test code

--- test/unit/queue.test.js
+++ test/unit/queue.test.js
@@ -1,33 +1,40 @@
 import Queue from "../../src/js/Components/Queue";
 
-jest.mock("../../src/js/Components/Queue");
-beforeEach(() => {
-    Queue.mockClear()
-})
+// 모듈 테스트
+let queue = new Queue([1, 2, 3, 4, 5]);
 
+describe("클래스 Queue 테스트", () => {
+  beforeEach(() => {
+    queue = new Queue([1, 2, 3, 4, 5]);
+  });
 
-it('클래스 호출 체크', () => {
-    const queue = new Queue();
-    expect(Queue).toHaveBeenCalledTimes(1);
+  test("인스턴스 생성", () => {
+    expect(queue.getList).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  test("_push method 확인", () => {
+    jest.spyOn(queue, "_push");
+    queue._push(6);
+    expect(queue.getList).toEqual([6, 1, 2, 3, 4, 5]);
+  });
+
+  test("_pop method 확인", () => {
+    jest.spyOn(queue, "_pop");
+    queue._pop();
+    expect(queue.getList).toEqual([1, 2, 3, 4]);
+  });
+
+  test("_pop method argument(2) 확인", () => {
+    jest.spyOn(queue, "_pop");
+    queue._pop(2);
+    expect(queue.getList).toEqual([1, 2, 3]);
+  });
+
+  test("length property 확인", () => {
+    expect(queue.length).toBe(5);
+  });
+
+  test("empty property 확인", () => {
+    expect(queue.empty).not.toBe(true);
+  });
 });
-
-it('클래스 생성 확인', () => {
-    const mockedClassQueue = new Queue([]);
-    expect(mockedClassQueue).toBeTruthy()
-});
-
-it('클래스 호출 확인 및 값 확인', () => {
-    expect(Queue).not.toHaveBeenCalled()
-    const queue = new Queue([1, 2])
-    expect(Queue).toHaveBeenCalled()
-
-    expect(Queue.mock.calls[0][0]).toEqual([1, 2])
-});
-
-// it("클래스 메소드 확인", () => {
-//     const queue = new Queue([1, 2])
-//     expect(Queue.mock.calls[0][0]).toEqual([1, 2])
-// })
-
-
-
